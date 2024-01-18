09dbb5be9cea0e67d6c7ba3c668a5a3dd80b42e5
Vinícius <viniciustrindadeworks@gmail.com>
2024-01-18 08:18:28 -0300

    Criando uma função prompt

---
+++ funcao-prompt.js
@@ -0,0 +1,7 @@
+var nome = prompt("Insira seu nome");
+var idade = prompt("Insira sua idade");
+var cidade = prompt("Insira sua cidade");
+
+console.log("O nome é: "+nome);
+console.log("A idade é: "+idade);
+console.log("A cidade é: "+cidade);
